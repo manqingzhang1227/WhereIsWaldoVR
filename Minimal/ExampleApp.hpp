706496c1b4d37d998123837c32d8a0a5163aa90a
Manqing <maz026@ucsd.edu>
2019-04-17 16:01:13 -0700

    finishing text

--- Minimal/ExampleApp.hpp
+++ Minimal/ExampleApp.hpp
@@ -1,9 +1,3 @@
-//
-// Created by Manqing (Stefanie) Zhang on 2019-04-15.
-//
-
-
-
 #ifndef WHEREISWALDOVR_EXAMPLEAPP_H
 #define WHEREISWALDOVR_EXAMPLEAPP_H
 
@@ -12,78 +6,96 @@
 #include "RiftApp.hpp"
 #include "Scene.hpp"
 #include <GL/glew.h>
+#include <ft2build.h>
+#include FT_FREETYPE_H
 
 
 // An example application that renders a simple cube
 class ExampleApp : public RiftApp {
-  std::shared_ptr<ColorCubeScene> cubeScene;
+  std::shared_ptr <ColorCubeScene> cubeScene;
+
+
   bool gameStarted;
+
+
   double startTime;
+
+
   int points;
+
 
 public:
   ExampleApp() {
-	  points = 0;
-	  startTime = -1;
-	  gameStarted = false;
+    points = 0;
+    startTime = -1;
+    gameStarted = false;
   }
 
 protected:
   void initGl() override {
     RiftApp::initGl();
-    glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
-    glEnable(GL_DEPTH_TEST);
-    ovr_RecenterTrackingOrigin(_session);
-    cubeScene = std::shared_ptr<ColorCubeScene>(new ColorCubeScene());
+    glClearColor( 1.0f, 1.0f, 1.0f, 0.0f );
+    glEnable( GL_DEPTH_TEST );
+    ovr_RecenterTrackingOrigin( _session );
+    //cubeScene = std::shared_ptr <ColorCubeScene>( new ColorCubeScene() );
+    cubeScene = std::shared_ptr <ColorCubeScene>(
+      new ColorSphereSceneWithText );
   }
+
 
   void shutdownGl() override {
     cubeScene.reset();
   }
 
-  void renderScene(const glm::mat4& projection, const glm::mat4& headPose) override {
-	// Get Current Input State
-	ovrInputState inputState;
-	ovr_GetInputState(_session, ovrControllerType_Touch, &inputState);
-	if (inputState.IndexTrigger[ovrHand_Right] > 0.5f && !gameStarted) {
-		startTime = ovr_GetTimeInSeconds();
-		gameStarted = true;
-		std::cout << " starting at time  " << ovr_GetTimeInSeconds()  << std::endl;
-	}
-	//std::cout << ovr_GetTimeInSeconds() << " vs " << startTime << std::endl;
-	if (ovr_GetTimeInSeconds() - startTime > 60.0 && gameStarted && startTime != -1 ) {
-		std::cout << "Game Over! You got " << points << " points." << std::endl;
-		gameStarted = false;
-		points = 0;
-	}
 
-	
-	// get the general state hmdState
-	double ftiming = ovr_GetPredictedDisplayTime(_session, 0);
-	ovrTrackingState hmdState = ovr_GetTrackingState(_session,
-		ftiming, ovrTrue);
-	// Get the state of hand poses
-	ovrPoseStatef handPoseState = hmdState.HandPoses[ovrHand_Right];
-	//Get the hand pose position.
-	glm::vec3 controllerPosition = ovr::toGlm(
-		handPoseState.ThePose.Position);
+  void renderScene( const glm::mat4 &projection,
+                    const glm::mat4 &headPose ) override {
+    // Get Current Input State
+    ovrInputState inputState;
+    ovr_GetInputState( _session, ovrControllerType_Touch, &inputState );
+    if( inputState.IndexTrigger[ovrHand_Right] > 0.5f && !gameStarted ) {
+      startTime = ovr_GetTimeInSeconds();
+      gameStarted = true;
+      std::cout << " starting at time  " << ovr_GetTimeInSeconds() << std::endl;
+    }
+    //std::cout << ovr_GetTimeInSeconds() << " vs " << startTime << std::endl;
+    if( ovr_GetTimeInSeconds() - startTime > 60.0 && gameStarted &&
+        startTime != -1 ) {
+      std::cout << "Game Over! You got " << points << " points." << std::endl;
+      gameStarted = false;
+      points = 0;
+    }
 
-	ovr_GetInputState(_session, ovrControllerType_Touch, &inputState);
-	if (inputState.IndexTrigger[ovrHand_Right] > 0.5f)
-	{
-		//std::cout << "Touched Trigger Button" << std::endl;
-		if (cubeScene->checkOverlap(controllerPosition) && gameStarted) {
-			std::cout << "1 Point Added" << std::endl;
-			std::cout << "You have " << 60 - (ovr_GetTimeInSeconds() - startTime) << " seconds left." << std::endl;
-			std::cout << "Current score: " << points << "." << std::endl;
-			std::cout <<  std::endl;
 
-			points++;
-		}
-	}
-	cubeScene->render(projection, glm::inverse(headPose), controllerPosition);
-	
+    // get the general state hmdState
+    double ftiming = ovr_GetPredictedDisplayTime( _session, 0 );
+    ovrTrackingState hmdState = ovr_GetTrackingState( _session,
+                                                      ftiming, ovrTrue );
+    // Get the state of hand poses
+    ovrPoseStatef handPoseState = hmdState.HandPoses[ovrHand_Right];
+    //Get the hand pose position.
+    glm::vec3 controllerPosition = ovr::toGlm(
+      handPoseState.ThePose.Position );
+
+    ovr_GetInputState( _session, ovrControllerType_Touch, &inputState );
+    if( inputState.IndexTrigger[ovrHand_Right] > 0.5f ) {
+      //std::cout << "Touched Trigger Button" << std::endl;
+      if( cubeScene->checkOverlap( controllerPosition ) && gameStarted ) {
+        std::cout << "1 Point Added" << std::endl;
+        std::cout << "You have " << 60 - ( ovr_GetTimeInSeconds() - startTime )
+                  << " seconds left." << std::endl;
+        std::cout << "Current score: " << points << "." << std::endl;
+        std::cout << std::endl;
+
+        points++;
+      }
+    }
+
+    cubeScene->render( projection, glm::inverse( headPose ),
+                       controllerPosition );
+
   }
 };
+
 
 #endif //WHEREISWALDOVR_EXAMPLEAPP_H
--- Minimal/Scene.hpp
+++ Minimal/Scene.hpp
@@ -7,6 +7,7 @@
 #ifndef WHEREISWALDOVR_SCENE_H
 #define WHEREISWALDOVR_SCENE_H
 
+
 #include <vector>
 #include "shader.h"
 #include "Cube.h"
@@ -21,81 +22,233 @@
 #include "Mesh.h"
 #include "Model.h"
 
+#include "text.h"
+
+
+// a class for building and rendering cubes
+class ColorSphereSceneWithText {
+
+  // Program
+  std::vector <glm::mat4> instance_positions;
+
+
+  GLuint instanceCount;
+
+
+  GLuint shaderID;
+
+
+  Model* sphere;
+
+
+  GLuint textShaderID;
+
+
+  Text* text;
+
+
+  GLuint lightedInstanceIndex;
+
+
+  const unsigned int GRID_SIZE{ 5 };
+
+
+  const float SPACING{ 0.14f };
+
+
+public:
+  ColorSphereSceneWithText() {
+    // Create a cube of cubes
+    sphere = new Model( "sphere.obj" );
+
+    for( float z = 0; z < GRID_SIZE; z++ ) {
+      for( float y = 0; y < GRID_SIZE; y++ ) {
+        for( float x = 0; x < GRID_SIZE; x++ ) {
+          float xpos = ( x - ( GRID_SIZE / 2.0f ) ) * SPACING;
+          float ypos = ( y - ( GRID_SIZE / 2.0f ) ) * SPACING;
+          float zpos = ( z - ( GRID_SIZE / 2.0f ) ) * SPACING;
+          vec3 relativePosition = vec3( xpos, ypos, zpos );
+          if( relativePosition == vec3( 0 ) ) {
+            continue;
+          }
+          glm::mat4 transfMat =
+            glm::translate( glm::mat4( 1.0f ), relativePosition ) *
+            glm::scale( glm::mat4( 1.0f ), vec3( 0.035, 0.035, 0.035 ) );
+          instance_positions.push_back( transfMat );
+        }
+      }
+    }
+
+
+    instanceCount = instance_positions.size();
+    lightedInstanceIndex = rand() % instanceCount;
+    // in the range 0 to instanceCount
+
+
+    // Shader Program
+    shaderID = LoadShaders( "shader.vert", "shader.frag" );
+
+    textShaderID = LoadShaders( "textShader.vert", "textShader.frag" );
+
+  }
+
+
+  void highlightRandomSphere() {
+    lightedInstanceIndex = rand() % instanceCount;
+    // in the range 0 to instanceCount
+  }
+
+
+  bool checkOverlap( const glm::vec3 &controllerPosition ) {
+    glm::vec4 highlightedCubePosition_h =
+      instance_positions.at( lightedInstanceIndex ) *
+      vec4( 0.0f, 0.0f, 0.0f, 1.0f );
+    glm::vec3 highlightedCubePosition =
+      vec3( highlightedCubePosition_h.x, highlightedCubePosition_h.y,
+            highlightedCubePosition_h.z ) / highlightedCubePosition_h.w;
+    float distance = glm::length(
+      controllerPosition - highlightedCubePosition );
+
+    if( distance < 0.035f ) {
+      highlightRandomSphere();
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+
+  void render( const glm::mat4 &projection, const glm::mat4 &view,
+               const glm::vec3 &controllerPosition ) {
+
+    for( int i = 0; i < instanceCount; i++ ) {
+      glm::mat4 toWorld = instance_positions[i] *
+                          glm::scale( glm::mat4( 1.0f ), glm::vec3( 0.5f ) );
+      sphere->Draw( shaderID, projection, view, toWorld,
+                    ( lightedInstanceIndex == i ) );
+    }
+    glm::mat4 controller_transform =
+      glm::translate( glm::mat4( 1.0f ), controllerPosition ) *
+      glm::scale( glm::mat4( 1.0f ), vec3( 0.0175, 0.0175, 0.0175 ) );
+    sphere->Draw( shaderID, projection, view, controller_transform, 2 );
+
+    //text->RenderText()
+    //render the text
+
+  }
+
+
+};
+
 
 // a class for building and rendering cubes
 class ColorCubeScene {
 
   // Program
-  std::vector<glm::mat4> instance_positions;
+  std::vector <glm::mat4> instance_positions;
+
+
   GLuint instanceCount;
+
+
   GLuint shaderID;
-  Model *sphere;
+
+
+  Model* sphere;
+
 
   GLuint lightedInstanceIndex;
 
-  const unsigned int GRID_SIZE{5};
+
+  const unsigned int GRID_SIZE{ 5 };
+
+
   const float SPACING{ 0.14f };
+
 
 public:
   ColorCubeScene() {
     // Create a cube of cubes
     {
-		
-		sphere = new Model("sphere.obj");
 
-		for (float z = 0; z < GRID_SIZE; z ++) {
-			for (float y = 0; y < GRID_SIZE; y ++) {
-				for (float x = 0; x < GRID_SIZE; x ++) {
-					float xpos = (x - (GRID_SIZE / 2.0f)) * SPACING;
-					float ypos = (y - (GRID_SIZE / 2.0f)) * SPACING;
-					float zpos = (z - (GRID_SIZE / 2.0f) ) * SPACING;
-					vec3 relativePosition = vec3(xpos, ypos, zpos);
-					if (relativePosition == vec3(0)) {
-					  continue;
-					}
-					glm::mat4 transfMat = glm::translate(glm::mat4(1.0f) , relativePosition) * glm::scale(glm::mat4(1.0f), vec3(0.035, 0.035, 0.035));
-					instance_positions.push_back(transfMat);
-				}
-			}
-		  }
-		}
+      sphere = new Model( "sphere.obj" );
 
-		instanceCount = instance_positions.size();
-		lightedInstanceIndex = rand() % instanceCount;         // in the range 0 to instanceCount
-
-
-		// Shader Program
-		shaderID = LoadShaders("shader.vert", "shader.frag");
-
-  }
-
-  void highlightRandomSphere( ) {
-	lightedInstanceIndex = rand() % instanceCount;         // in the range 0 to instanceCount
-  }
-
-  bool checkOverlap(const glm::vec3& controllerPosition) {
-	  glm::vec4 highlightedCubePosition_h = instance_positions.at(lightedInstanceIndex)*vec4(0.0f,0.0f,0.0f,1.0f);
-	  glm::vec3 highlightedCubePosition = vec3(highlightedCubePosition_h.x, highlightedCubePosition_h.y, highlightedCubePosition_h.z) / highlightedCubePosition_h.w;
-	  float distance = glm::length(controllerPosition - highlightedCubePosition);
-
-	  if (distance < 0.035f) {
-		  highlightRandomSphere();
-		  return true;
-	  }
-	  else {
-		  return false;
-	  }
-  }
-
-  void render(const glm::mat4& projection, const glm::mat4& view, const glm::vec3& controllerPosition) {
-
-    for (int i = 0; i < instanceCount; i++) {
-		glm::mat4 toWorld = instance_positions[i] * glm::scale(glm::mat4(1.0f), glm::vec3(0.5f));
-		sphere->Draw(shaderID, projection, view, toWorld, (lightedInstanceIndex == i));
+      for( float z = 0; z < GRID_SIZE; z++ ) {
+        for( float y = 0; y < GRID_SIZE; y++ ) {
+          for( float x = 0; x < GRID_SIZE; x++ ) {
+            float xpos = ( x - ( GRID_SIZE / 2.0f ) ) * SPACING;
+            float ypos = ( y - ( GRID_SIZE / 2.0f ) ) * SPACING;
+            float zpos = ( z - ( GRID_SIZE / 2.0f ) ) * SPACING;
+            vec3 relativePosition = vec3( xpos, ypos, zpos );
+            if( relativePosition == vec3( 0 ) ) {
+              continue;
+            }
+            glm::mat4 transfMat =
+              glm::translate( glm::mat4( 1.0f ), relativePosition ) *
+              glm::scale( glm::mat4( 1.0f ), vec3( 0.035, 0.035, 0.035 ) );
+            instance_positions.push_back( transfMat );
+          }
+        }
+      }
     }
-	glm::mat4 controller_transform = glm::translate(glm::mat4(1.0f), controllerPosition) * glm::scale(glm::mat4(1.0f), vec3(0.0175, 0.0175, 0.0175));
-	sphere->Draw(shaderID, projection, view, controller_transform, 2);
+
+    instanceCount = instance_positions.size();
+    lightedInstanceIndex =
+      rand() % instanceCount;         // in the range 0 to instanceCount
+
+
+    // Shader Program
+    shaderID = LoadShaders( "shader.vert", "shader.frag" );
+
   }
+
+
+  void highlightRandomSphere() {
+    lightedInstanceIndex =
+      rand() % instanceCount;         // in the range 0 to instanceCount
+  }
+
+
+  bool checkOverlap( const glm::vec3 &controllerPosition ) {
+    glm::vec4 highlightedCubePosition_h =
+      instance_positions.at( lightedInstanceIndex ) *
+      vec4( 0.0f, 0.0f, 0.0f, 1.0f );
+    glm::vec3 highlightedCubePosition =
+      vec3( highlightedCubePosition_h.x, highlightedCubePosition_h.y,
+            highlightedCubePosition_h.z ) / highlightedCubePosition_h.w;
+    float distance = glm::length(
+      controllerPosition - highlightedCubePosition );
+
+    if( distance < 0.035f ) {
+      highlightRandomSphere();
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+
+  void render( const glm::mat4 &projection, const glm::mat4 &view,
+               const glm::vec3 &controllerPosition ) {
+
+    for( int i = 0; i < instanceCount; i++ ) {
+      glm::mat4 toWorld = instance_positions[i] *
+                          glm::scale( glm::mat4( 1.0f ), glm::vec3( 0.5f ) );
+      sphere->Draw( shaderID, projection, view, toWorld,
+                    ( lightedInstanceIndex == i ) );
+    }
+    glm::mat4 controller_transform =
+      glm::translate( glm::mat4( 1.0f ), controllerPosition ) *
+      glm::scale( glm::mat4( 1.0f ), vec3( 0.0175, 0.0175, 0.0175 ) );
+    sphere->Draw( shaderID, projection, view, controller_transform, 2 );
+
+    //render the text
+
+  }
+
+
 };
 
 
